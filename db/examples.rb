ada184f9f544257b89c3229a94bda7350be26468
novakw267 <novakw267@gmail.com>
2017-02-18 16:39:19 -0500

    I added some example data

--- db/examples.rb
+++ db/examples.rb
@@ -17,3 +17,37 @@
 #                password: 'abc123',
 #                password_confirmation: nil)
 # end
+User.transaction do
+  %w(lauren maddie jeff max alec kate brian gary anna).each do |name|
+    email = "#{name}@#{name}.com"
+    next if User.exists? email: email
+    User.create(email: email,
+                password: 'abc123',
+                password_confirmation: 'abc123')
+  end
+end
+
+Profile.transaction do
+  %w(lauren maddie jeff max alec kate brian gary anna).each do |name|
+    email = "#{name}@#{name}.com"
+    user = User.where(email: email).first
+    profile_params = {
+      user_id: user.id,
+      username: name,
+      favorite_genre: 'metal',
+      favorite_band: 'the jackson 4'
+    }
+    next if Profile.exists? profile_params
+    Profile.create! profile_params
+  end
+end
+
+# Workout.transaction do
+#   %w(2kBenchmark 3668Benchmark 5kSS 10kSS 2500SS).each do |name|
+#     workout_params = {
+#       name: name
+#     }
+#     next if Workout.exists? workout_params
+#     Workout.create! workout_params
+#   end
+# end
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20170218200636) do
     t.string   "favorite_band",  null: false
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.integer  "user_id"
+    t.integer  "user_id",        null: false
     t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
