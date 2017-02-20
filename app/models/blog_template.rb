3608bffc4e2abf9b5707583d971dd61536cf2663
novakw267 <novakw267@gmail.com>
2017-02-20 18:56:29 -0500

    Still working on blog template create

--- app/models/blog_template.rb
+++ app/models/blog_template.rb
@@ -3,6 +3,7 @@
 # :nodoc:
 class BlogTemplate < ApplicationRecord
   belongs_to :profile
+  delegate :blog_templates, to: :profiles
   validates :blog_title, presence: true
   validates :subject, presence: true
   validates :body, presence: true
